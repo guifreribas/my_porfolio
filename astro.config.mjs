10d465090241fac10ef5420a77e1ab09c25e79a6
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-17 16:31:29 +0200

    feat: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: "es",
     locales: ["ca", "es", "en"],
   },
+
+  integrations: [tailwind()],
 });
