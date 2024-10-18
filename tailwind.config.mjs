b09e3d3c1213629fb3f8fbc347bcd49ae068551c
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-18 15:58:25 +0200

    feat: enhance tailwind config with custom colors, fonts, keyframes, and animations for improved styling options

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"gf-yellow": "#e6c339",
+				"gf-brown": "#a69553",
+				"gf-blue": "#0b0d3f",
+				"gf-purple": "#545791",
+				"gf-gray": "#66614d",
+			},
+			fontFamily: {
+				body: ["NunitoSans", "sans-serif"],
+				headers: ["Onest", "sans-serif"],
+			},
+			keyframes: {
+				"slide-in": {
+					"0%": { transform: "translateY(20%)", opacity: 0 },
+					"100%": { transform: "translateY(0)", opacity: 1 },
+				},
+			},
+			animation: {
+				"slide-in": "slide-in 0.6s ease-in-out forwards",
+			},
+		},
 	},
 	plugins: [],
-}
+};
